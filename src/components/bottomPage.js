25f17ea7405c0f2e67ecd51d040e97c19bc06dd8
Евгений Чип <evgesha.chip.98@mail.ru>
2021-01-12 22:06:06 +0100

    is 12th day? переписал код для dots

--- src/components/bottomPage.js
+++ src/components/bottomPage.js
@@ -22,11 +22,11 @@ export const BottomPage = () =>{
         if(e.target.value>=2012){setState({slide:Bg3})}
    }
     return(
-           <div id={"anchor3"} style={{backgroundImage:`url(${state.slide})`}} className="Page bottomPage">
+           <div  style={{backgroundImage:`url(${state.slide})`}} className="Page bottomPage">
             
             
                    <input type="range" className="bottomRange" onChange={NextItem} min={1988} max={2026} value={state.currentBgIndex} defaultValue={2016} step={1} />            
-               <div className="input_values">
+               <div id={"anchor3"} className="input_values">
                    <span >1998  2009  2016</span>
                </div>
            </div>
--- src/components/pagination/dots.js
+++ src/components/pagination/dots.js
@@ -1,32 +1,42 @@
 import React,{useState} from "react";
 
 export const Dots = ()=>{
-     const [state1,setActive1] = useState(true);
-     const [state2,setActive2] = useState(false);
-     const [state3,setActive3] = useState(false);
+     const [state1,setActive1] = useState(1);
+     
+    const arrDots = [
+        {
+            name:1,
+            href:"#anchor1"
+        },
+        {
+            name:2,
+            href:"#anchor2"
+        },
+        {
+            name:3,
+            href:"#anchor3"
+        },
 
-    const trig = () =>{
-        setActive1(true)
-        setActive2(false)
-        setActive3(false)
-    }
+]
+    
 
-    const trig2 = () =>{
-        setActive1(false)
-        setActive2(true)
-        setActive3(false)
-    }
-    const trig3 = () =>{
-        setActive1(false)
-        setActive2(false)
-        setActive3(true)
-    }
+  const handlerDot = (e)=>{
+      setActive1(e)
+  }
 
     return(
     <div className="Pagination">
-        <a name={1} href="#anchor1" onClick={trig} className={state1?"dots active":"dots"}> </a>
-        <a name={2} href="#anchor2" onClick={trig2} className={state2?"dots active":"dots"}> </a>
-        <a name={3} href="#anchor3" onClick={trig3} className={state3?"dots active":"dots"}> </a>
+        {arrDots.map((e)=>
+        <a key={e.name} name={e.name}
+           href={e.href} 
+           onClick={()=>handlerDot(e.name)} 
+           className={state1===e.name?"dots active":"dots"}> 
+        </a>)}
+
+        
+        {/* <a name={1} href="#anchor1" onClick={()=>handlerDot(e)} className={state1===e?"dots active":"dots"}> </a>
+        <a name={2} href="#anchor2" onClick={()=>handlerDot(e)} className={state1===e?"dots active":"dots"}> </a>
+        <a name={3} href="#anchor3" onClick={()=>handlerDot(e)} className={state1===e?"dots active":"dots"}> </a> */}
     </div>
         
     
